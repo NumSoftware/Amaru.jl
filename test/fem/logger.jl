98f33cf3f16a72d9cf543271b0d789669e0c1fe0
Raul Durand <raul.durand@gmail.com>
2018-05-16 14:03:07 -0400

    logger updated

--- test/fem/logger.jl
+++ test/fem/logger.jl
@@ -11,22 +11,22 @@ node_mon1 = Logger(:node, :(x==1 && y==1 && z==1) )
 node_mon2 = Logger(:node, :(x==1 && y==0 ) )
 node_mon3 = Logger(:node, 1 )
 
-nodes_mon1 = GroupLogger(:nodes, :(x==1 && y==1 ) )
+nodes_mon1 = GroupLogger(:node, :(x==1 && y==1 ) )
 
-faces_mon1 = Logger(:faces, :(z==1) )
-faces_mon2 = Logger(:faces, :(z==1) )
-edges_mon1 = Logger(:edges, :(z==1) )
-edges_mon2 = Logger(:edges, :(z==1) )
+faces_mon1 = Logger(:face, :(z==1) )
+faces_mon2 = Logger(:face, :(z==1) )
+edges_mon1 = Logger(:edge, :(z==1) )
+edges_mon2 = Logger(:edge, :(z==1) )
 
 ip_mon1   = Logger(:ip, 1 )
 ip_mon2   = Logger(:ip, :(x>0.5 && y>0.5 && z>0.5) )
 
-ips_mon1   = GroupLogger(:ips, :(x>0.5 && y>0.5) )
+ips_mon1   = GroupLogger(:ip, :(x>0.5 && y>0.5) )
 
-mons = [ node_mon1, node_mon2, node_mon3, nodes_mon1, faces_mon1, faces_mon2,
+logs = [ node_mon1, node_mon2, node_mon3, nodes_mon1, faces_mon1, faces_mon2,
         edges_mon1, edges_mon2, ip_mon1, ip_mon2, ips_mon1 ]
 
-dom = Domain(mesh, mat, mons)
+dom = Domain(mesh, mat, logs)
 
 bcs = [
        BC(:node, :(z==0), :(ux=0, uy=0, uz=0 )),
