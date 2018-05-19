afaab5f276238b16ea362962186ca2d756ce780b
Raul Durand <raul.durand@gmail.com>
2018-05-19 06:41:29 -0300

    added file to clean output files

---
+++ test/clean.jl
@@ -0,0 +1,15 @@
+# Remove output files from tests
+
+path  = dirname(@__FILE__)
+files = readdir(path)
+
+for (root, dirs, files) in walkdir(path)
+    for file in files
+        ext = split(file*".", ".")[2]
+        if ext in ["vtk", "dat"]
+            fullname = joinpath(root, file)
+            println("  deleting file $fullname")
+            rm(fullname)
+        end
+    end
+end
