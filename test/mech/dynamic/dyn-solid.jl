330c1e8a4c66aff9aa2e12201e8f06382a94a690
Raul Durand <raul.durand@gmail.com>
2018-05-22 19:40:47 -0400

    GLob removed

--- test/mech/dynamic/dyn-solid.jl
+++ test/mech/dynamic/dyn-solid.jl
@@ -31,6 +31,3 @@ bcs = [
 
 dynsolve!(dom, bcs, time_span=0.1, nincs=50, verbose=true, filekey="dyn", nouts=50, autoinc=true, alpha=4.2038, beta=174.2803e-6)
 #dynsolve!(dom, bcs, time_span=0.1, nincs=50, verbose=true, filekey="dyn", nouts=100, autoinc=true)
-
-using Glob
-rm.(glob("*.vtk"))
