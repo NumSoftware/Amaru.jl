4646d4ba479bd3106ea6773050ae91b03c321b1b
Bryan German Pantoja Rosero <35929939+bgpantojar@users.noreply.github.com>
2018-12-18 14:04:02 -0500

    Actualization runtest
    The files name of two new joint test have been add.

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,6 +32,8 @@ FILES = [
     # Static analysis: joint models
     "mech/joint/joint1d.jl",
     "mech/joint/joint2d.jl",
+    "mech/joint/joint2d_tag.jl",
+    "mech/joint/joint3d_tag.jl",
 
     # Static analysis: embeddeed and semi-embedded elements
     "mech/inset/embedded.jl",
