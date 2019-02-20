f3b8e3c036a3fe6e65c03da78d29903d207b5aa1
Raul Durand <raul.durand@gmail.com>
2019-02-20 09:32:22 -0300

    file renamed

--- src/analysis-data.jl
+++ src/analysis-data.jl
@@ -1,17 +1,22 @@
 # This file is part of Amaru package. See copyright license in https://github.com/NumSoftware/Amaru
 
-mutable struct SharedAnalysisData
+mutable struct AnalysisData
     ndim::Int              # Analysis dimension
     model_type::Symbol     # Analysis type (eg. :general, :plane_stress, :plane_strain, etc.
     thickness::Float64     # Model thickness
     t::Float64             # Time in time dependent analysis
-    function SharedAnalysisData()
+    nstage::Int            # Current stage
+    ninc::Int              # Current increment
+    nout::Int              # Current output file number
+    function AnalysisData()
         this = new()
         this.ndim = 3
-        this.model_type = :general
-        this.t = 0.0
+        this.model_type = :general # plane_stress, axisymmetric
         this.thickness = 1.0
+        this.t = 0.0
+        this.nstage = 0 # current stage
+        this.ninc   = 0 # current increment
+        this.nout   = 0 # output files counter
         return this
     end
 end
-
