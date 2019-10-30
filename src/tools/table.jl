0429eaf93480861830a2b3e0f6607441b7457f4e
Raul Durand <raul.durand@gmail.com>
2019-10-30 20:41:15 +1000

    DTable fixed

--- src/tools/table.jl
+++ src/tools/table.jl
@@ -6,86 +6,110 @@ export DTable, DBook, push!, keys, getindex, save, loadtable, loadbook
 
 
 # DTable object
-const HeaderType = Union{Symbol,AbstractString}
+const KeyType = Union{Symbol,AbstractString}
+#const ItemType  = Union{Int64,Float64,AbstractString}
+const ColType = Array{T,1} where T
 
 mutable struct DTable
-    data    ::Array{Array{Float64,1},1}
-    colindex::Dict{Symbol,Int} # Data index
-    fields  ::Array{Symbol,1}
+    columns ::Array{ColType,1}
+    colindex::OrderedDict{String,Int} # Data index
+    header::Array{String,1}
     function DTable()
         this = new()
-        this.data     = [ ]
-        this.colindex = Dict() 
-        this.fields   = []
+        this.columns  = []
+        this.colindex = OrderedDict() 
+        this.header   = []
         return this
     end
-    function DTable(header::Array{<:HeaderType,1})
+    function DTable(header::Array)
         this = new()
-        this.data     = [ Float64[] for s in header ]
-        this.colindex = Dict( Symbol(key)=>i for (i,key) in enumerate(header) )
-        this.fields   = Symbol.(header)
+        header = vec(header)
+        this.columns  = [ [] for s in header ]
+        this.colindex = OrderedDict( string(key)=>i for (i,key) in enumerate(header) )
+        this.header   = string.(header)
         return this
     end
 end
 
 
-function DTable(header::Array{<:HeaderType,1}, data::Array{Array{Float64,1},1})
+function DTable(header::Array, columns::Array{<:ColType,1})
     this      = DTable(header)
     nfields   = length(header)
-    ncols     = length(data)
+    ncols     = length(columns)
     nfields  != ncols && error("DTable: header and number of data columns do not match")
-    this.data = deepcopy(data)
+    this.columns = deepcopy(columns)
     return this
 end
 
 
-function DTable(header::Array{<:HeaderType,1}, matrix::Array{Float64,2})
-    this      = DTable(header)
-    nfields   = length(header)
-    ncols     = size(matrix,2)
-    nfields  != ncols && error("DTable: header and number of data columns do not match")
-    this.data = [ matrix[:,i] for i=1:nfields ]
+function DTable(header::Array, matrix::Array{T,2} where T)
+    this   = DTable(header)
+    nkeys  = length(header)
+    ncols  = size(matrix,2)
+    nkeys != ncols && error("DTable: header and number of data columns do not match")
+    types = [ typeof(matrix[1,i]) for i=1:ncols ]
+    this.columns = [ convert(Array{types[i],1}, matrix[:,i]) for i=1:ncols ]
     return this
 end
 
+
+mutable struct DBook
+    tables::Array{DTable, 1}
+    function DBook()
+        this = new()
+        this.tables = DTable[]
+        return this
+    end
+end
 
 
 import Base.push!
-function push!(table::DTable, row::Array{Float64,1})
-    @assert length(table.fields)==length(row)
-    for (i,val) in enumerate(row)
-        push!(table.data[i], val)
+function push!(table::DTable, row::Array{T,1} where T)
+    @assert length(table.colindex)==length(row)
+    
+    if length(table.columns[1])==0
+        table.columns = [ typeof(v)[v] for v in row  ]
+    else
+        for (i,val) in enumerate(row)
+            @show typeof(table.columns[i])
+            push!(table.columns[i], val)
+        end
     end
 end
 
 
+function push!(book::DBook, table::DTable)
+    push!(book.tables, table)
+end
+
 function keys(table::DTable)
-    return table.fields
+    return keys(table.colindex)
 end
 
 function Base.push!(table::DTable, dict::AbstractDict)
-    if length(table.data)==0
-        table.data     = [ Float64[v] for (k,v) in dict ]
-        table.colindex = Dict( key=>i for (i,key) in enumerate(keys(dict)) )
-        table.fields   = collect(keys(dict))
+    if length(table.columns)==0
+        table.columns  = [ typeof(v)[v] for (k,v) in dict ]
+        table.colindex = OrderedDict( string(key)=>i for (i,key) in enumerate(keys(dict)) )
+        table.header   = string.(keys(dict))
     else
-        nrows = length(table.data[1])
+        nrows = length(table.columns[1])
         for (k,v) in dict
             # Add data
-            colindex = get(table.colindex, k, 0)
+            colindex = get(table.colindex, string(k), 0)
             if colindex==0
                 # add new column
                 new_col = zeros(nrows)
                 push!(new_col, v)
-                push!(table.data, new_col)
-                push!(table.fields, k)
-                table.colindex[k] = length(table.data)
+                push!(table.columns, new_col)
+                table.colindex[string(k)] = length(table.columns)
+                push!(table.header, string(k))
             else
-                push!(table.data[colindex], v)
+                push!(table.columns[colindex], v)
             end
         end
+
         # Add zero for missing values if any
-        for col in table.data
+        for col in table.columns
             if length(col)==nrows
                 push!(col, 0.0)
             end
@@ -93,47 +117,85 @@ function Base.push!(table::DTable, dict::AbstractDict)
     end
 end
 
-function Base.getindex(table::DTable, key::HeaderType)
-    return table.data[table.colindex[key]]
+function Base.getindex(table::DTable, key::KeyType)
+    return table.columns[table.colindex[string(key)]]
 end
 
-function Base.getproperty(table::DTable, key::Symbol)
-    fields = getfield(table, :fields) # use of getfield instead of dot operator to avoid stack overflow
-
-    if key in fields
-        idx = getfield(table, :colindex)[key]
-        return getfield(table, :data)[idx]
-    else
-        return getfield(table, key)
-    end
-end
-
-function Base.getindex(table::DTable, keys::Array{<:HeaderType,1})
-    data = [ table[key] for key in keys ]
-    subtable = DTable(keys, data)
+function Base.getindex(table::DTable, keys::Array{<:KeyType,1})
+    columns = [ table[string(key)] for key in keys ]
+    subtable = DTable(keys, columns)
     return subtable
 end
 
+function Base.getindex(table::DTable, rowindex::Int, colon::Colon)
+    row = []
+    for j=1:length(table.header)
+        push!(row, table.columns[j][rowindex])
+    end
 
-function save(table::DTable, filename::String; verbose::Bool=true)
-    format = split(filename*".", ".")[2]
-    format != "dat" && error("save DTable: filename should have \"dat\" extension")
+    return row
+end
 
-    f  = open(filename, "w")
-    nc = length(table.fields)             # number of fields (columns)
-    nr = nc>0 ? length(table.data[1]) : 0 # number of rows
+function Base.lastindex(table::DTable, idx::Int)
+    length(table.columns)==0 && error("DTable: use of 'end' in an empty table")
+    return length(table.columns[1])
+end
+
+function Base.getindex(book::DBook, index::Int)
+    return book.tables[index]
+end
+
+function Base.lastindex(book::DBook)
+    return length(book.tables)
+end
+
+# TODO: Check this function
+function Base.iterate(book::DBook, state=(nothing,1) )
+    table, idx = state
+    if idx<=length(book.tables)
+        return (book.tables[idx], (book.tables[i+1], idx+1))
+    else
+        return nothing
+    end
+end
+
+sprintf(fmt, args...) = @eval @sprintf($fmt, $(args...))
+
+# TODO: Improve column width for string items
+#function save(table::DTable, filename::String; verbose::Bool=true, digits::Array{Int,1}=[])
+function save(table::DTable, filename::String; verbose::Bool=true, digits::Array=[])
+    format = split(filename, ".")[end]
+    format in ("dat","json","tex") || error("save DTable: invalid extension $format")
+
+    local f::IOStream
+    try
+        f  = open(filename, "w") 
+    catch err
+        @warn "DTable: File $filename could not be opened for writing."
+        return
+    end
+
+
+    nc = length(table.colindex)             # number of cols
+    nr = nc>0 ? length(table.columns[1]) : 0 # number of rows
 
     if format=="dat"
-        # print header
-        for i=1:nc
-            @printf(f, "%12s", table.fields[i])
+        for (i,key) in enumerate(keys(table.colindex))
+            @printf(f, "%12s", key)
             print(f, i!=nc ? "\t" : "\n")
         end
 
         # print values
         for i=1:nr
             for j=1:nc
-                @printf(f, "%12.6e", table.data[j][i])
+                item = table.columns[j][i]
+                if typeof(item)<:AbstractFloat
+                    @printf(f, "%12.5e", item)
+                elseif typeof(item)<:Integer
+                    @printf(f, "%12d", item) 
+                else
+                    @printf(f, "%12s", item)
+                end
                 print(f, j!=nc ? "\t" : "\n")
             end
         end
@@ -148,69 +210,99 @@ function save(table::DTable, filename::String; verbose::Bool=true)
 
         verbose && printstyled("  file $filename written (DTable)\n", color=:cyan)
     end
+    
+    if format=="tex"
+        # widths calculation
+        header = keys(table.colindex)
+        widths = length.(header) 
+        types  = eltype.(table.columns)
+
+        if length(digits)==0
+            digits = repeat([4], nc)
+        end
+        @assert length(digits)==nc
+
+        for (i,col) in enumerate(table.columns)
+            etype = types[i]
+            if etype<:AbstractFloat
+                widths[i] = max(widths[i], 12)
+            elseif etype<:Integer
+                widths[i] = max(widths[i], 6)
+            elseif etype<:AbstractString
+                widths[i] = max(widths[i], maximum(length.(col)))
+            else
+                widths[i] = max(widths[i], maximum(length.(string.(col))))
+            end
+        end
+
+        # printing header
+        println(f, raw"\begin{tabular}{", "c"^nc, "}" )
+        println(f, raw"    \toprule")
+        print(f, "    ")
+        for (i,key) in enumerate(header)
+            etype = types[i]
+            width = widths[i]
+            if etype<:Real
+                print(f, lpad(key, width))
+            else
+                print(f, rpad(key, width))
+            end
+            i<nc && print(f, " & ")
+        end
+        println(f, raw" \\\\")
+
+        # printing body
+        println(f, raw"    \hline")
+        for i=1:nr
+            print(f, "    ")
+            for j=1:nc
+                etype = types[j]
+                item = table.columns[j][i]
+                width = widths[j]
+                if etype<:AbstractFloat
+                    #item = @sprintf("%12.3f", item)
+                    dig = digits[j]
+                    if isnan(item)
+                        item = "-"
+                    else
+                        item = sprintf("%$width.$(dig)f", item)
+                    end
+                    print(f, lpad(string(item), width))
+                elseif etype<:Integer
+                    item = @sprintf("%6d", item) 
+                    print(f, lpad(item, width))
+                elseif etype<:AbstractString
+                    print(f, rpad(item, width))
+                else
+                    str = string(item)
+                    print(f, rpad(item, width))
+                end
+                j<nc && print(f, " & ")
+            end
+            println(f, raw" \\\\")
+        end
+        println(f, raw"    \bottomrule")
+
+        # printing ending
+        println(f, raw"\end{tabular}")
+    end
 
     close(f)
     return nothing
 end
 
 
-function loadtable(filename::String, delim='\t')
-    format = split(filename*".", ".")[2]
-    format != "dat" && error("loadtable: filename should have \"dat\" extension")
-
-    if format=="dat"
-        data, headstr = readdlm(filename, delim, header=true, use_mmap=false)
-        fields = [ Symbol(strip(field)) for field in vec(headstr) ]
-
-        table = DTable(fields , data)
-        return table
-    end
-end
-
-
-
-mutable struct DBook
-    tables::Array{DTable, 1}
-    function DBook()
-        this = new()
-        this.tables = DTable[]
-        return this
-    end
-end
-
-
-function push!(book::DBook, table::DTable)
-    push!(book.tables, table)
-end
-
-
-function Base.getindex(book::DBook, index::Int)
-    return book.tables[index]
-end
-
-function Base.lastindex(book::DBook)
-    return length(book.tables)
-end
-
-#Base.start(book::DBook) = 1
-#Base.next(book::DBook, idx::Int) = book.tables[idx], idx+1
-#Base.done(book::DBook, idx::Int) = idx == length(book.tables)
-function Base.iterate(book::DBook, state=(nothing,1) )
-    table, idx = state
-    if idx<=length(book.tables)
-        return (book.tables[idx], (book.tables[i+1], idx+1))
-    else
-        return nothing
-    end
-end
-
-
-
 function save(book::DBook, filename::String; verbose::Bool=true)
-    format = split(filename*".", ".")[2]
+    format = split(filename, ".")[end]
     format != "dat" && error("save DBook: filename should have \"dat\" extension")
 
-    f  = open(filename, "w") 
+    local f::IOStream
+    try
+        f  = open(filename, "w") 
+    catch err
+        @warn "DBook: File $filename could not be opened for writing."
+        return
+    end
 
     if format=="json"
         # generate dictionary
@@ -225,22 +317,22 @@ function save(book::DBook, filename::String; verbose::Bool=true)
 
         for (k,table) in enumerate(book.tables)
 
-            nc = length(table.fields)             # number of fields (columns)
-            nr = nc>0 ? length(table.data[1]) : 0 # number of rows
+            nc = length(table.colindex)              # number of cols
+            nr = nc>0 ? length(table.columns[1]) : 0 # number of rows
 
             # print table label
-            print(f, "Table (snapshot=$k, rows=$nr)\n")
+            print(f, "Table (snapshot=$(k), rows=$nr)\n")
 
             # print header
-            for i=1:nc
-                @printf(f, "%12s", table.fields[i])
+            for (i,key) in enumerate(keys(table.colindex))
+                @printf(f, "%12s", key)
                 print(f, i!=nc ? "\t" : "\n")
             end
 
             # print values
             for i=1:nr
                 for j=1:nc
-                    @printf(f, "%12.6e", table.data[j][i])
+                    @printf(f, "%12.5e", table.columns[j][i])
                     print(f, j!=nc ? "\t" : "\n")
                 end
             end
@@ -254,8 +346,22 @@ function save(book::DBook, filename::String; verbose::Bool=true)
 
 end
 
+
+function loadtable(filename::String, delim='\t')
+    format = split(filename, ".")[end]
+    format != "dat" && error("loadtable: filename should have \"dat\" extension")
+
+    if format=="dat"
+        matrix, headstr = readdlm(filename, delim, header=true, use_mmap=false)
+        table = DTable(strip.(headstr), matrix)
+        return table
+    end
+end
+
+
 function loadbook(filename::String)
-    format = split(filename*".", ".")[2]
+    delim = "\t"
+    format = split(filename, ".")[end]
     format != "dat" && error("loadbook: filename should have \"dat\" extension")
 
     f      = open(filename, "r")
@@ -263,22 +369,33 @@ function loadbook(filename::String)
     if format=="dat"
         lines = readlines(f)
         header_expected = false
-        for line in lines
-            strip(line)=="" && continue
+        for (i,line) in enumerate(lines)
             items = split(line)
+            length(items)==0 && strip(line)=="" && continue
+
             if items[1]=="Table"
                 header_expected = true
                 continue
             end
             if header_expected # add new table
-                fields = [ key for key in split(line) ]
-                push!(book.tables, DTable(fields))
+                header = [ key for key in split(line, delim) ]
+                push!(book.tables, DTable(header))
                 header_expected = false
                 continue
             end
 
             length(book.tables) == 0 && error("loadbook: Wrong file format. Use loadtable() to read a table")
-            row = parse.(Float64, items)
+            row = []
+            for item in items
+                try
+                    char1 = item[1]
+                    isnumeric(char1) || char1 in ('+','-') ?  push!(row, Meta.parse(item)) : push!(row, item)
+                    #isnumeric(item[1]) ?  push!(row, Meta.parse(item)) : push!(row, item)
+                catch err
+                    @error "loadbook: Error while reading value '$item' at line $i"
+                    throw(err)
+                end
+            end
             push!(book.tables[end], row) # udpate newest table
         end
     end
@@ -287,25 +404,91 @@ function loadbook(filename::String)
     return book
 end
 
+# TODO: Improve display. Include column datatype
 function Base.show(io::IO, table::DTable)
-    nc = length(table.colindex)  # number of fields (columns)
-    nr = length(table.data[1])   # number of rows
+    if length(table.columns)==0
+        print("DTable()")
+        return
+    end
+    nc = length(table.colindex)     # number of fields (columns)
+    nr = length(table.columns[1])   # number of rows
 
-    matrix = [ table.data[j][i] for i=1:nr, j=1:nc ]
-    header = reshape(Text.([:row; table.fields]), (1, nc+1))
-    print(io, "DTable $(nr)x$nc:\n")
-    #Base.showarray(io, [header; collect(1:nr) matrix], false, header=false)
-    io = IOContext(io, :compact => true)
-    Base.print_matrix(io, [header; collect(1:nr) matrix])
+    if nr==0
+        print("DTable()")
+        return
+    end
+
+    header = keys(table.colindex)
+    widths = length.(header) 
+    types  = eltype.(table.columns)
+    for (i,col) in enumerate(table.columns)
+        etype = types[i]
+        if etype<:AbstractFloat
+            widths[i] = max(widths[i], 12)
+        elseif etype<:Integer
+            widths[i] = max(widths[i], 6)
+        elseif etype<:AbstractString
+            widths[i] = max(widths[i], maximum(length.(col)))
+        else
+            widths[i] = max(widths[i], maximum(length.(string.(col))))
+        end
+    end
+
+    print(" │ ")
+    for (i,key) in enumerate(header)
+        etype = types[i]
+        width = widths[i]
+        if etype<:Real
+            print(lpad(key, width))
+        else
+            print(rpad(key, width))
+        end
+        print(" │ ")
+    end
+    println()
+
+    visible_rows = 30
+    half_vrows = div(visible_rows,2)
+
+    # print values
+    for i=1:nr
+        if i>half_vrows && nr-i>=half_vrows
+            i==half_vrows+1 && println(" ⋮")
+            continue
+        end
+
+        print(" │ ")
+        for j=1:nc
+            etype = types[j]
+            item = table.columns[j][i]
+            if etype<:AbstractFloat
+                item = @sprintf("%12.5e", item)
+                print(lpad(item, widths[j]))
+            elseif etype<:Integer
+                item = @sprintf("%6d", item) 
+                print(lpad(item, widths[j]))
+            elseif etype<:AbstractString
+                str = item
+                print(rpad(item, widths[j]))
+            else
+                str = string(item)
+                print(rpad(item, widths[j]))
+            end
+            print(" │ ")
+        end
+        i<nr && println()
+    end
+
 end
 
 function Base.show(io::IO, book::DBook)
     print(io, "DBook (tables=$(length(book.tables))):\n")
+    n = length(book.tables)
     for (k,table) in enumerate(book.tables)
         # print table label
-        nitems = length(table.data[1])
-        #print(io, "  Table (snapshot=$k, items=$nitems):\n")
-        str = "  "*replace(string(table), "\n","\n  ")
-        print(io, str, "\n")
+        nitems = length(table.columns[1])
+        print(io, " Table (snapshot=$(k), rows=$nitems):\n")
+        str = string(table)
+        k<n && print(io, str, "\n")
     end
 end
