d857c85ac75a781a1530537cdda91688b04b57dc
Pedro Filipe Cunha <pedro_flc@hotmail.com>
2020-05-07 00:31:35 -0300

    att

--- src/mech/elem/mech-plate-mzc-4node.jl
+++ src/mech/elem/mech-plate-mzc-4node.jl
@@ -21,104 +21,6 @@ end
 
 matching_shape_family(::Type{PlateMZC}) = SOLID_SHAPE
 
-#=
-function plate_B_matrix(elem::PlateMZC)
-        C  = get_coords(elem)
-        a  = C[2,1]-C[1,1] # element length in X direction
-        b  = C[2,2]-C[2,1] # element length in Y direction
-
-        gauss_x = zeros(4,1)
-        gauss_y = zeros(4,1)
-
-        gauss_x[1] = -1/sqrt(3);
-        gauss_y[1] = -1/sqrt(3);
-
-        gauss_x[2] =  1/sqrt(3);
-        gauss_y[2] =-1/sqrt(3);
-
-        gauss_x[3] = 1/sqrt(3);
-        gauss_y[3] = 1/sqrt(3);
-
-        gauss_x[4] =-1/sqrt(3);
-        gauss_y[4] = 1/sqrt(3);
-
-
-  for igaus = 1 : 4
-        x = gauss_x[igaus] # x = Local X coordinate of the Gauss point
-        y = gauss_y[igaus] # y = Local Y coordinate of the Gauss point
-        #Bb = zeros(3, 12) # strain-displacement matrix
-
-        d2N = zeros(4,3)
-        d2N[1,1] = 3*( x - x*y )/(4*a^2);
-        d2N[2,1] = 3*(-x + x*y )/(4*a^2);
-        d2N[3,1] = 3*(-x - x*y )/(4*a^2);
-        d2N[4,1] = 3*( x + x*y )/(4*a^2);
-
-        d2N[1,2] = 3*( y - x*y )/(4*b^2);
-        d2N[2,2] = 3*( y + x*y )/(4*b^2);
-        d2N[3,2] = 3*(-y - x*y )/(4*b^2);
-        d2N[4,2] = 3*(-y + x*y )/(4*b^2);
-
-        d2N[1,3] = 2*(  1/2 - 3*x^2/8 - 3*y^2/8)/(a*b);
-        d2N[2,3] = 2*( -1/2 + 3*x^2/8 + 3*y^2/8)/(a*b);
-        d2N[3,3] = 2*(  1/2 - 3*x^2/8 - 3*y^2/8)/(a*b);
-        d2N[4,3] = 2*( -1/2 + 3*x^2/8 + 3*y^2/8)/(a*b);
-        #------------------------------------------------------
-        d2NN = zeros(4,3)
-        d2NN[1,1] = ( (3*a*x - 3*a*x*y - a + a*y)/4 )/a^2;
-        d2NN[2,1] = ( (3*a*x - 3*a*x*y + a - a*y)/4 )/a^2;
-        d2NN[3,1] = ( (3*a*x + 3*a*x*y + a + a*y)/4 )/a^2;
-        d2NN[4,1] = ( (3*a*x + 3*a*x*y - a - a*y)/4 )/a^2;
-
-        d2NN[1,2] = 0;
-        d2NN[2,2] = 0;
-        d2NN[3,2] = 0;
-        d2NN[4,2] = 0;
-
-        d2NN[1,3] = 2*( -3/8*a*x^2 + a*x/4 + a/8 )/(a*b);
-        d2NN[2,3] = 2*( -3/8*a*x^2 - a*x/4 + a/8 )/(a*b);
-        d2NN[3,3] = 2*(  3/8*a*x^2 + a*x/4 - a/8 )/(a*b);
-        d2NN[4,3] = 2*(  3/8*a*x^2 - a*x/4 - a/8 )/(a*b);
-        #------------------------------------------------------
-        d2NNN = zeros(4,3)
-        d2NNN[1,1] = 0;
-        d2NNN[2,1] = 0;
-        d2NNN[3,1] = 0;
-        d2NNN[4,1] = 0;
-
-        d2NNN[1,2] = ( (3*b*y - 3*b*x*y - b + b*x)/4 )/b^2;
-        d2NNN[2,2] = ( (3*b*y + 3*b*x*y - b - b*x)/4 )/b^2;
-        d2NNN[3,2] = ( (3*b*y + 3*b*x*y + b + b*x)/4 )/b^2;
-        d2NNN[4,2] = ( (3*b*y - 3*b*x*y + b - b*x)/4 )/b^2;
-
-        d2NNN[1,3] = 2*( -3/8*b*y^2 + b*y/4 + b/8 )/(a*b);
-        d2NNN[2,3] = 2*(  3/8*b*y^2 - b*y/4 - b/8 )/(a*b);
-        d2NNN[3,3] = 2*(  3/8*b*y^2 + b*y/4 - b/8 )/(a*b);
-        d2NNN[4,3] = 2*( -3/8*b*y^2 - b*y/4 + b/8 )/(a*b);
-        #-----------------------------------------------------
-        bmat_1  = [ -d2N[1,1] -d2NN[1,1] -d2NNN[1,1]
-                    -d2N[1,2] -d2NN[1,2] -d2NNN[1,2]
-                    -d2N[1,3] -d2NN[1,3] -d2NNN[1,3]];
-
-        bmat_2  = [ -d2N[2,1] -d2NN[2,1] -d2NNN[2,1]
-                    -d2N[2,2] -d2NN[2,2] -d2NNN[2,2]
-                    -d2N[2,3] -d2NN[2,3] -d2NNN[2,3]];
-
-        bmat_3  = [ -d2N[3,1] -d2NN[3,1] -d2NNN[3,1]
-                    -d2N[3,2] -d2NN[3,2] -d2NNN[3,2]
-                    -d2N[3,3] -d2NN[3,3] -d2NNN[3,3]];
-
-        bmat_4  = [ -d2N[4,1] -d2NN[4,1] -d2NNN[4,1]
-                    -d2N[4,2] -d2NN[4,2] -d2NNN[4,2]
-                    -d2N[4,3] -d2NN[4,3] -d2NNN[4,3]];
-
-        Bb = [bmat_1 bmat_2 bmat_3 bmat_4];
-
-     end
-
-     return
-=#
-
 function D_matrix(elem::PlateMZC)
     th     = elem.env.thickness
     coef = elem.mat.E*th^3/(12*(1-elem.mat.nu^2));
@@ -162,8 +64,8 @@ function elem_map(elem::PlateMZC)::Array{Int,1}
     #    dof_keys = (:ux, :uy, :uz, :rx, :ry, :rz) # VERIFICAR
     #end
 
-
     dof_keys = (:uz, :rx, :ry)
+
     vcat([ [node.dofdict[key].eq_id for key in dof_keys] for node in elem.nodes]...)
 
 end
@@ -180,16 +82,7 @@ end
 =#
 
 function elem_stiffness(elem::PlateMZC)
-#=
-    C  = get_coords(elem)
-    L  = norm(C[2,:]-C[1,:])
-    L2 = L*L
-    L3 = L*L*L
-    mat = elem.mat
-    EA = mat.E*mat.A
-    EI = mat.E*mat.I
 
-=#
     nnodes = length(elem.nodes)
     th     = elem.env.thickness
     C  = get_coords(elem)
@@ -199,14 +92,10 @@ function elem_stiffness(elem::PlateMZC)
     d2N = zeros(4,3)
     d2NN = zeros(4,3)
     d2NNN = zeros(4,3)
-
     K_elem = zeros( nnodes*3 , nnodes*3 )
 
     D_mat = D_matrix(elem)
 
-    C  = get_coords(elem)
-    a  = C[2,1]-C[1,1] # element length in X direction
-    b  = C[2,2]-C[2,1] # element length in Y direction
 
     gauss_x = zeros(4,1)
     gauss_y = zeros(4,1)
@@ -292,8 +181,10 @@ function elem_stiffness(elem::PlateMZC)
 
         Bb = [bmat_1 bmat_2 bmat_3 bmat_4];
 
-        # K_elem = K_elem + bmat'*D_mat*bmat_b*a*b
-        K_elem = Bb'*D_mat*Bb*a*b
+
+        K_elem += Bb'*D_mat*Bb*a*b
+        #println(K_elem)
+        #println(size(K_elem))
     end
     map = elem_map(elem)
 
