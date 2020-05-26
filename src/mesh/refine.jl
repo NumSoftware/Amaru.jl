577d2b392f017171558e2b16f135e19b822b74d7
Raul Durand <raul.durand@gmail.com>
2020-05-26 00:23:11 -0500

    hrefine HEX8 added

--- src/mesh/refine.jl
+++ src/mesh/refine.jl
@@ -1,10 +1,11 @@
 function hrefine(mesh::Mesh; n=2, verbose=true)
-    msh = Mesh()
+    n==1 && return copy(mesh)
+    newmesh = Mesh()
 
     for cell in mesh.elems
-        if cell.shape==TRI3
-            coords =get_coords(cell.nodes)
+        coords =get_coords(cell.nodes)
 
+        if cell.shape==TRI3
             p_arr = Array{Node}(undef, n+1, n+1)
             for j = 1:n+1
                 for i = 1:n+1
@@ -13,16 +14,15 @@ function hrefine(mesh::Mesh; n=2, verbose=true)
                     s = (1.0/n)*(j-1)
 
                     N = cell.shape.func([r, s])
-                    C = N'*coords
+                    C = round.(N'*coords, digits=8)
                     if i==1 || j==1 || i+j==n+2
-                        C = round.(C, digits=8)
-                        p =get_node(msh._pointdict, C)
+                        p = get_node(newmesh._pointdict, C)
                         if p==nothing
-                            p = Node(C); push!(msh.nodes, p)
-                            msh._pointdict[hash(p)] = p
+                            p = Node(C); push!(newmesh.nodes, p)
+                            newmesh._pointdict[hash(p)] = p
                         end
                     else
-                        p = Node(C); push!(msh.nodes, p)
+                        p = Node(C); push!(newmesh.nodes, p)
                     end
                     p_arr[i,j] = p
                 end
@@ -36,24 +36,73 @@ function hrefine(mesh::Mesh; n=2, verbose=true)
                     p3 = p_arr[i  , j+1]
 
                     cell1 = Cell(cell.shape, [p1, p2, p3], tag=cell.tag)
-                    push!(msh.elems, cell1)
+                    push!(newmesh.elems, cell1)
 
                     if i+j < n+1
                         p4 = p_arr[i+1, j+1]
                         cell2 = Cell(cell.shape, [p2, p4, p3], tag=cell.tag)
-                        push!(msh.elems, cell2)
+                        push!(newmesh.elems, cell2)
                     end
                 end
             end
+            continue
         end
+
+        if cell.shape==HEX8
+            p_arr = Array{Node}(undef, n+1, n+1, n+1)
+            for k = 1:n+1
+                for j = 1:n+1
+                    for i = 1:n+1
+                        r = (2.0/n)*(i-1) - 1.0
+                        s = (2.0/n)*(j-1) - 1.0
+                        t = (2.0/n)*(k-1) - 1.0
+                        N = cell.shape.func([r, s, t])
+                        C = round.(N'*coords, digits=8)
+                        if i in (1, n+1) || j in (1, n+1) || k in (1, n+1)
+                            p =get_node(newmesh._pointdict, C)
+                            if p==nothing
+                                p = Node(C); push!(newmesh.nodes, p)
+                                newmesh._pointdict[hash(p)] = p
+                            end
+                        else
+                            p = Node(C); push!(newmesh.nodes, p)
+                        end
+                        p_arr[i,j,k] = p
+                    end
+                end
+            end
+
+            for k = 1:n
+                for j = 1:n
+                    for i = 1:n
+                        p1 = p_arr[i  , j  , k  ]
+                        p2 = p_arr[i+1, j  , k  ]
+                        p3 = p_arr[i+1, j+1, k  ]
+                        p4 = p_arr[i  , j+1, k  ]
+                        p5 = p_arr[i  , j  , k+1]
+                        p6 = p_arr[i+1, j  , k+1]
+                        p7 = p_arr[i+1, j+1, k+1]
+                        p8 = p_arr[i  , j+1, k+1]
+
+                        cell = Cell(cell.shape, [p1, p2, p3, p4, p5, p6, p7, p8], tag=cell.tag)
+                        push!(newmesh.elems, cell)
+                    end
+                end
+            end
+            continue
+        end
+
+        error("hrefine: Cannot refine mesh containing elements of type $(cell.shape.name)")
+
     end
-    fixup!(msh, reorder=true)
-    return msh
+
+    fixup!(newmesh, reorder=true)
+    return newmesh
 end
 
 
 function prefine(mesh::Mesh; n=2, verbose=true)
-    msh = Mesh()
+    newmesh = Mesh()
 
     for cell in mesh.elems
         if cell.shape==TRI3
@@ -65,18 +114,18 @@ function prefine(mesh::Mesh; n=2, verbose=true)
                 N = cell.shape.func(R)
                 C = coords'*N
                 C = round.(C, digits=8)
-                p =get_node(msh._pointdict, C)
+                p =get_node(newmesh._pointdict, C)
                 if p==nothing
                     p = Node(C);
-                    push!(msh.nodes, p)
-                    msh._pointdict[hash(p)] = p
+                    push!(newmesh.nodes, p)
+                    newmesh._pointdict[hash(p)] = p
                 end
                 push!(points, p)
             end
             newcell = Cell(NS, points, tag=cell.tag)
-            push!(msh.elems, newcell)
+            push!(newmesh.elems, newcell)
         end
     end
-    fixup!(msh, reorder=true)
-    return msh
+    fixup!(newmesh, reorder=true)
+    return newmesh
 end
