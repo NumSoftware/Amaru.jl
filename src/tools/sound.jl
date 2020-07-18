ca3c44ee1e5a7a5d5affb51c2b9d009e41917316
Raul Durand <raul.durand@gmail.com>
2020-07-18 13:20:53 -0500

    fixed error on windows

--- src/tools/sound.jl
+++ src/tools/sound.jl
@@ -2,18 +2,21 @@
 export sound_alert
 
 function sound_alert()
-    cmd = `beep`
-    if Sys.islinux()
-        file = joinpath(@__DIR__, "alert.oga")
-        cmd = `paplay $file`
-    end
-    if Sys.iswindows()
-        cmd = `echo ^G`
-    end
+    try
+        cmd = `beep`
+        if Sys.islinux()
+            file = joinpath(@__DIR__, "alert.oga")
+            cmd = `paplay $file`
+        end
+        if Sys.iswindows()
+            cmd = `cmd /c 'echo \^G'`
+        end
 
-    out = Pipe()
-    err = Pipe()
-    run(pipeline(ignorestatus(cmd), stdout=out, stderr=err))
-    close(out.in)
-    close(err.in)
+        out = Pipe()
+        err = Pipe()
+        run(pipeline(ignorestatus(cmd), stdout=out, stderr=err))
+        close(out.in)
+        close(err.in)
+    catch err
+    end
 end
