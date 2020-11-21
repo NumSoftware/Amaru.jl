da55b2baac045646cff27eb000054d1b0adeb5a2
Raul Durand <raul.durand@gmail.com>
2020-11-21 11:43:24 -0300

    old cplot removed

--- src/plot/cplot.jl
+++ src/plot/cplot.jl
@@ -94,7 +94,7 @@ function cplot(data::Array{<:NamedTuple},
                ylabel           = L"y",
                legendloc        = "best",
                xbins            = 10,
-               ybins            = 10,
+               ybins            = 8,
                grid             = true,
                figsize          = (3,2),
                legendexpand     = false,
@@ -103,8 +103,8 @@ function cplot(data::Array{<:NamedTuple},
                ylim             = nothing,
                xscale           = "linear",
                yscale           = "linear",
-               xmult            = 1,
-               ymult            = 1,
+               xmult            = 1.0,
+               ymult            = 1.0,
                ticksinside      = true,
                fontsize         = 6,
                legendfontsize   = 0,
@@ -116,6 +116,11 @@ function cplot(data::Array{<:NamedTuple},
                tagfontsize      = 6,
                tagcolor         = "black",
                tagalign         = true,
+               
+               y2bins           = 8,
+               y2lim            = nothing,
+               y2scale          = "linear",
+               y2mult           = 1.0,
               )
 
     headline("Chart plotting")
@@ -127,7 +132,7 @@ function cplot(data::Array{<:NamedTuple},
     hint(options, level=3)
 
     hint("Arguments and optional arguments per curve:", level=2)
-    options = "x, y, color, ls, lw, marker, ms, mfc, label, tag, tagpos, tagloc, tagdist, tagfontsize, tagcolor, tagalign"
+    options = "x, y, color, ls, lw, marker, ms, mfc, label, tag, tagpos, tagloc, tagdist, tagfontsize, tagcolor, tagalign, at_y2"
     hint(options, level=3)
 
     @eval import PyPlot:plt, matplotlib, figure
@@ -312,7 +317,7 @@ function cplot(data::Array{<:NamedTuple},
         end
     end
 
-    # line labels
+    # line tags
     # from Axes to Data coords:  (ax.transAxes+ax.transData.inverted()).transform([x,y])
     # from Data to Axes coords:  (ax.transData+ax.transAxes.inverted()).transform([x,y])
     for (i, line) in enumerate(data)
@@ -436,6 +441,13 @@ function cplot(data::Array{<:NamedTuple},
             transform=ax.transAxes
             )
     end
+
+    # second axis
+    # ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
+    # ax2.plot(t, data2, color=color)
+    # ax2.set_ylim([0, 2.8])
+    # ax2.set_ylabel("Average stress [MPa]")  # we already handled the x-label with ax1
+    # ax2.tick_params(axis='y', labelcolor=color)
      
     # show or save plot
     if filename=="" 
@@ -447,339 +459,3 @@ function cplot(data::Array{<:NamedTuple},
     end
 
 end
-
-
-
-
-#const CHART_ARGS = Dict{String, Any}()
-#=
-"""
-    newchart(kwargs...)
-
-Configures the ploting area for a `PyPlot` line chart.
-
-# Keyword arguments
-
-`xlabel         = "\$x\$"` : x label
-
-`ylabel         = "\$y\$"` : y label
-
-`lw             = 0.7` : line width
-
-`xbins          = 6` : number of bins in x
-
-`ybins          = 6` : number of bins in y
-
-`grid           = false` : grid
-
-`figsize        = (3,2)` : size of plot
-
-`fontsize       = 7` : font size
-
-`legendloc      = "best"` : legend location in plot
-
-`legendfontsize = 0` : defaults to fontsize
-
-`legendexpand   = false` : expanded version
-
-`labelspacing   = 0.5` : spacing between legend labels
-
-`ticksinside    = true` : put ticks inside plot
-
-# Example
-
-The following example shows the function usage:
-
-```
-using Amaru, PyPlot
-
-newchart()
-X = 1:10
-Y = rand(10)
-Z = rand(10)
-
-plot(X, Y, "k", label="label1")
-plot(X, Z, "b", label="label2")
-
-savechart("filename.pdf")
-```
-"""
-function newchart(; 
-                  xlabel         = "\$x\$",
-                  ylabel         = "\$y\$",
-                  lw             = 0.7,
-                  xbins          = 6,
-                  ybins          = 6,
-                  grid           = false,
-                  figsize        = (3,2),
-                  fontsize       = 7,
-                  legendloc      = "best",
-                  legendfontsize = 0,
-                  legendexpand   = false,
-                  labelspacing   = 0.5,
-                  ticksinside    = true
-              )
-
-    #CHART_ARGS["legendloc"]    = legendloc
-    #CHART_ARGS["legendexpand"] = legendexpand
-    #CHART_ARGS["labelspacing"] = labelspacing
-
-    @eval import PyPlot:plt,gca
-
-    # Configure plot
-    plt.close("all")
-
-    plt.rc("font", family="STIXGeneral", size=fontsize)
-    plt.rc("mathtext", fontset="cm")
-    plt.rc("lines", scale_dashes=true)
-
-    plt.rc("xtick", labelsize=7)
-    plt.rc("ytick", labelsize=7)
-    plt.rc("lines", lw=0.7)
-    plt.rc("lines", markersize=2)
-    plt.rc("axes" , linewidth=0.5)
-    plt.rc("figure", figsize=(3, 2))
-    legendfontsize==0 && (legendfontsize=fontsize)
-    plt.rc("legend", fontsize=legendfontsize)
-
-    grid && plt.grid( color="lightgrey", ls="dotted", lw=0.3)
-    ax = plt.axes()
-    plt.locator_params(axis="x", nbins=xbins)
-    plt.locator_params(axis="y", nbins=ybins)
-
-    # Set limits
-    !isnan(xmax) && plt.xlim(right=xmax)
-    !isnan(ymax) && plt.ylim(top=ymax)
-
-    # Print axes labels
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    # Tick marks direction
-    ax.tick_params(axis="y", direction="in")
-    ax.tick_params(axis="x", direction="in")
-
-    return nothing
-end
-
-
-"""
-    savechart(filename; kwargs...)
-
-Saves the current `PyPlot` line chart.
-
-# Arguments
-
-`filename` : output file
-
-# Keyword arguments
-
-`showlegend = true` : controls if legend is shown
-
-`format     = "pdf"` : output file format
-
-"""
-function savechart(filename; showlegend=true, format="pdf")
-    legendloc    = CHART_ARGS["legendloc"]
-    legendexpand = CHART_ARGS["legendexpand"]
-    labelspacing = CHART_ARGS["labelspacing"]
-    plt.ioff()
-    ax = gca()
-    if showlegend
-        mode = legendexpand ? "expand" : nothing
-        ncol = length(ax.lines)
-
-        if legendloc=="top"
-            leg = plt.legend(loc="lower left", bbox_to_anchor=(-0.02, 1.01, 1.04, 0.2), edgecolor="k", ncol=ncol, mode=mode)
-        elseif legendloc=="right"
-            leg = plt.legend(loc="upper left", bbox_to_anchor=(1.01, 1), edgecolor="k")
-        elseif legendloc=="bottom"
-            leg = plt.legend(loc="upper left", bbox_to_anchor=(-0.02, -0.02, 1.04, -0.2), edgecolor="k", ncol=ncol, mode=mode)
-        else
-            leg = plt.legend(loc=legendloc, edgecolor="k", labelspacing=labelspacing)
-        end
-
-        frame = leg.get_frame()
-        frame.set_linewidth(0.5)
-    end
-    plt.savefig(filename, bbox_inches="tight", pad_inches=0.01, format=format)
-end
-
-
-function showchart(showlegend=true, legendloc="best", labelspacing=0.5, legendexpand=false)
-    plt.ion()
-    plt.legend(loc=legendloc, edgecolor="k", labelspacing=labelspacing)
-    show()
-end
-=#
-
-
-function cplot0(X, Y, filename=""; xlabel="\$x\$", ylabel="\$y\$", lw=0.7, ls="-", ms=2, marker=nothing, color="", legend=[], legendloc="best",
-               xbins=6, ybins=6, grid=false, figsize=(3,2), legendexpand=false, ncol=0,
-               xlim = nothing,
-               ylim = nothing,
-               #xmin=NaN, xmax=NaN,
-               #ymin=NaN, ymax=NaN,
-               fontsize=7, legendfontsize=0, labelspacing=0.5)
-
-    @eval import PyPlot:plt, matplotlib, figure
-
-    # Fix labels
-    texlabels = Dict(
-                  "sxx"=>raw"$\sigma_{xx}$",
-                  "syy"=>raw"$\sigma_{yy}$",
-                  "szz"=>raw"$\sigma_{zz}$",
-                  "sxy"=>raw"$\sigma_{xy}$",
-                  "syz"=>raw"$\sigma_{yz}$",
-                  "sxz"=>raw"$\sigma_{xz}$",
-                  "exx"=>raw"$\varepsilon_{xx}$",
-                  "eyy"=>raw"$\varepsilon_{yy}$",
-                  "ezz"=>raw"$\varepsilon_{zz}$",
-                  "exy"=>raw"$\varepsilon_{xy}$",
-                  "eyz"=>raw"$\varepsilon_{yz}$",
-                  "exz"=>raw"$\varepsilon_{xz}$",
-                  "sigma" =>raw"$\sigma$",
-                  "tau"   =>raw"$\tau$",
-                  "lambda"=>raw"$\lambda$",
-                 )
-
-    labels = [ xlabel, ylabel ]
-    for (i,label) in enumerate(labels)
-        if haskey(texlabels, label)
-            label = get(texlabels, label, label)
-        else
-            !occursin(r"^\$.+\$$", label) && ( label = replace(label, r"(\w+)_(\w+)" =>  s"$\1_{\2}$") )
-            if occursin(r"^\$.+\$$", label)
-                label = replace(label, r"\b(sigma|tau|lambda)(\b|_)" => s"\\\1\2")
-            end
-        end
-        labels[i] = label
-    end
-    xlabel, ylabel = labels
-
-    # Configure plot
-    plt.close("all")
-
-    plt.rc("font", family="STIXGeneral", size=fontsize)
-    plt.rc("mathtext", fontset="cm")
-    plt.rc("lines", scale_dashes=true)
-
-    if filename!=""
-        plt.ioff()
-        plt.rc("xtick", labelsize=7)
-        plt.rc("ytick", labelsize=7)
-        plt.rc("lines", lw=0.7)
-        plt.rc("lines", markersize=2)
-        plt.rc("axes" , linewidth=0.5)
-        plt.rc("figure", figsize=(3, 2))
-        legendfontsize==0 && (legendfontsize=fontsize)
-        plt.rc("legend", fontsize=legendfontsize)
-    end
-
-    # Convert amtrices to array of vectors
-    size(X,2)>1 && ( X = [ X[:,j] for j=1:size(X,2) ] )
-    size(Y,2)>1 && ( Y = [ Y[:,j] for j=1:size(Y,2) ] )
-
-    # Convert vectors to array of one vector
-    eltype(X)<:Real && (X = [X] )
-    eltype(Y)<:Real && (Y = [Y] )
-    mx = length(X)
-    my = length(Y)
-    mx == 1 && ( X = repeat(X, my) )
-    my == 1 && ( Y = repeat(Y, mx) )
-    @assert length(X)==length(Y) "cplot: X and Y should have the same size"
-    m = max(mx, my)
-
-    # Fix markers
-    if typeof(marker)==String || marker==nothing
-        marker = repeat([marker], m)
-    end
-    @assert length(marker)==m
-
-    # Fix markers size
-    if typeof(ms) <: Real
-        ms = repeat([ms], m)
-    end
-    @assert length(ms)==m
-
-    # Fix line widths - lw
-    if typeof(lw) <: Real
-        lw = repeat([lw], m)
-    end
-    @assert length(lw)==m
-
-    # Fix line styles - ls
-    if typeof(ls)==String
-        ls = repeat([ls], m)
-    end
-    @assert length(ls)==m
-
-    # Fix line colors
-    if typeof(color)==String
-        if color==""
-            color = nothing
-        end
-        color = repeat([color], m)
-    end
-    @assert length(color)==m
-
-    # Fix legend labels
-    haslegend = true
-    if length(legend)==0
-        haslegend = false
-        legend = repeat([""],m)
-    end
-    @assert length(legend)==m
-
-
-    # Plot curves
-    for i=1:m
-        plt.plot(X[i], Y[i], marker=marker[i], ms=ms[i], color=color[i], lw=lw[i], ls=ls[i], label=legend[i])
-    end
-
-    grid && plt.grid( color="lightgrey", ls="dotted", lw=0.3)
-    ax = plt.axes()
-    plt.locator_params(axis="x", nbins=xbins)
-    plt.locator_params(axis="y", nbins=ybins)
-
-    # Set limits
-    xlim!=nothing && plt.xlim(xlim)
-    ylim!=nothing && plt.ylim(ylim)
-    #!isnan(xmax) && plt.xlim(right=xmax)
-    #!isnan(ymax) && plt.ylim(top=ymax)
-
-    # Print axes labels
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    # plot legend
-    if haslegend
-        mode = legendexpand ? "expand" : nothing
-        ncol = ncol==0 ? m : ncol
-
-        if legendloc=="top"
-            leg = plt.legend(loc="lower left", bbox_to_anchor=(-0.02, 1.01, 1.04, 0.2), edgecolor="k", ncol=ncol, mode=mode)
-        elseif legendloc=="right"
-            leg = plt.legend(loc="upper left", bbox_to_anchor=(1.01, 1), edgecolor="k")
-        elseif legendloc=="bottom"
-            leg = plt.legend(loc="upper left", bbox_to_anchor=(-0.02, -0.02, 1.04, -0.2), edgecolor="k", ncol=ncol, mode=mode)
-        else
-            leg = plt.legend(loc=legendloc, edgecolor="k", labelspacing=labelspacing)
-        end
-
-        frame = leg.get_frame()
-        frame.set_linewidth(0.5)
-    end
-
-    # show or save plot
-    if filename==""
-        plt.show()
-    else
-        plt.savefig(filename, bbox_inches="tight", pad_inches=0.01, format="pdf")
-    end
-
-    plt.close("all")
-end
-
-
-
