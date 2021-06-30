91cd24e40d381bfe14754b9c67c9e36268d298fd
Raul Durand <raul.durand@gmail.com>
2021-06-30 18:52:07 -0300

    docs

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+# Inside make.jl
+push!(LOAD_PATH,"../src/")
+using Amaru, Documenter
+
+makedocs(
+         sitename = "Amaru",
+         modules  = [Amaru],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/NumSoftware/Amaru",
+)
