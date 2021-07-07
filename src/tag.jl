5e827d8a56cdb5abf498c546a27b1ecb6eb393ce
Raul Durand <raul.durand@gmail.com>
2021-07-07 18:15:28 -0300

    docstring updated

--- src/tag.jl
+++ src/tag.jl
@@ -1,19 +1,20 @@
 # This file is part of Amaru package. See copyright license in https://github.com/NumSoftware/Amaru
 
 # Tag functions
-for T in (Node, AbstractCell, Ip)
+for T in (Node, Cell, Block, Element, Ip)
     @eval begin
-        tag!(object::$T, tag::String) = (object.tag = tag)
+        """
+            $(TYPEDSIGNATURES)
+
+        Tag a $($T) `object` by setting the `tag` string.
+        """
+        tag!(object::$T, tag::String) = (object.tag = tag; nothing)
+
+        """
+            $(SIGNATURES)
+
+        Tag all $($T) `objects` in an array using the `tag` string.
+        """
         tag!(objects::Array{<:$T,1}, tag::String) = for object in objects; object.tag = tag end
     end
 end
-
-@doc """
-    tag!(object, tag)
-
-Modify the field `object.tag` with the value `tag`.
-
-    tag!(itr, tag)
-Tag each object in `itr` with the value `tag`.
-""" tag!
-
