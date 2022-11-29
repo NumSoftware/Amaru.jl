6efe5dfa0bd21e6a874f9d5bfd39bacc4b34bb20
Raul Durand <raul.durand@gmail.com>
2022-11-28 21:29:08 -0300

    revolve fixed

--- test/mesh/extrude.jl
+++ test/mesh/extrude.jl
@@ -20,7 +20,7 @@ printstyled("\nExtrude normal to Mesh\n", color=:blue, bold=true)
 
 bl  = Block( [1 0 0; 1 1 0], n=6, cellshape=LIN3, tag="shell")
 mesh = Mesh(bl)
-mesh = revolve(mesh, minangle=45, maxangle=135, n=6, axis=[0,-1,0])
+mesh = revolve(mesh, minangle=45, maxangle=135, n=6, axis=[0,-1,0], base=[0,0,0])
 mesh = extrude(mesh, length=4.25, n=6, quiet=true)
 TR = @test length(mesh.elems) == 216
 println(TR)
