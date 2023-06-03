f705022254fe720eae72988d8fa2fc132f4a903e
Raul Durand <raul.durand@gmail.com>
2023-06-03 19:23:59 -0300

    error message

--- src/tag.jl
+++ src/tag.jl
@@ -2,6 +2,7 @@
 
 # Tag functions
 for T in (Node, Cell, Block, Element, Ip)
+    
     @eval begin
         """
             $(TYPEDSIGNATURES)
@@ -16,5 +17,9 @@ for T in (Node, Cell, Block, Element, Ip)
         Tag all $($T) `objects` in an array using the `tag` string.
         """
         tag!(objects::Array{<:$T,1}, tag::String) = for object in objects; object.tag = tag end
+        
     end
+    
 end
+
+tag!(::Nothing, tag::String) = error("There are no objects to apply tag \"$tag\". The tag! function received 'nothing'.")
