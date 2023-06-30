88d504b6571e79518b5644825a7102d0e8c0f9bd
Raul Durand <raul.durand@gmail.com>
2023-06-29 21:44:25 -0300

    properties added

---
+++ src/properties.jl
@@ -0,0 +1,12 @@
+# This file is part of Amaru package. See copyright license in https://github.com/NumSoftware/Amaru
+
+
+# Abstract type for element properties
+# ====================================
+
+"""
+`Propperties`
+
+Abstract type for a set of element properties.
+"""
+abstract type Properties end
