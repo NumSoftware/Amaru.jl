e4033851d467e0f74fec5996d1e212dbcd51a379
Raul Durand <raul.durand@gmail.com>
2023-07-19 23:06:02 -0300

    tm-combined added

---
+++ src/thermomech/mat/tm-combined.jl
@@ -0,0 +1,77 @@
+
+export TMCombined
+
+mutable struct TMCombinedState{S1,S2}<:IpState
+    env::ModelEnv
+    tstate::S1 # thermo
+    mstate::S2 # mech
+    function TMCombinedState{S1,S2}(env=ModelEnv()) where {S1,S2}
+        return new{S1,S2}(env, S1(env), S2(env))
+    end
+end
+
+
+function Base.getproperty(state::TMCombinedState, s::Symbol)
+    tstate = getfield(state, :tstate)
+    mstate = getfield(state, :mstate)
+    s==:tstate && return tstate
+    s==:mstate && return mstate
+    s==:env && return getfield(state, :env)
+    fields1 = propertynames(tstate)
+    fields2 = propertynames(mstate)
+    s in fields1 && return getfield(tstate, s)
+    s in fields2 && return getfield(mstate, s)
+    error("type TMCombinedState has no field $s")
+end
+
+
+mutable struct TMCombined{M1,M2}<:Material
+    tmat::M1 # thermo
+    mmat::M2 # mech
+
+    function TMCombined{M1,M2}(; params...) where {M1,M2}
+        tmat = M1(;params...)
+        mmat = M2(;params...)
+        
+        return new{M1,M2}(tmat, mmat)
+    end
+end
+
+# Type of corresponding state structure
+ip_state_type(::Type{TMCombined{M1,M2}}) where {M1,M2} = TMCombinedState{ip_state_type(M1), ip_state_type(M2)} 
+
+matching_elem_types(::Type{TMCombined{M1,M2}}) where {M1,M2} = (TMSolid, TMShell)
+
+
+function Base.getproperty(mat::TMCombined, s::Symbol)
+    tmat = getfield(mat, :tmat)
+    mmat = getfield(mat, :mmat)
+    s==:tmat && return tmat
+    s==:mmat && return mmat
+    fields1 = propertynames(tmat)
+    fields2 = propertynames(mmat)
+    s in fields1 && return getfield(tmat, s)
+    s in fields2 && return getfield(mmat, s)
+    error("type TMCombined has no field $s")
+end
+
+
+function calcD(mat::TMCombined, state::TMCombinedState, stressmodel::String=state.env.ana.stressmodel)
+    return calcD(mat.mmat, state.mstate, stressmodel)
+end
+
+function calcK(mat::TMCombined, state::TMCombinedState) # Hydraulic conductivity matrix
+    return calcK(mat.tmat, state.tstate)
+end
+
+function update_state!(mat::TMCombined, state::TMCombinedState, Δε::Array{Float64,1}, Δut::Float64, G::Array{Float64,1}, Δt::Float64, stressmodel::String=state.env.ana.stressmodel)
+    QQ = update_state!(mat.tmat, state.tstate, Δut, G, Δt)
+    Δσ, status = update_state!(mat.mmat, state.mstate, Δε, stressmodel)
+    return Δσ, QQ, status
+end
+
+function ip_state_vals(mat::TMCombined, state::TMCombinedState, stressmodel::String=state.env.ana.stressmodel)
+    vals1 = ip_state_vals(mat.tmat, state.tstate)
+    vals2 = ip_state_vals(mat.mmat, state.mstate, stressmodel)
+    return merge(vals1, vals2)
+end
