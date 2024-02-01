022889d13a60b46d4a4d0b4cda2a4155bdf01b0c
Raul Durand <raul.durand@gmail.com>
2024-02-01 16:41:08 -0300

    fixed missing mesh canvas

---
+++ src/plot/mesh-canvas.jl
@@ -0,0 +1,14 @@
+# This file is part of Amaru package. See copyright license in https://github.com/NumSoftware/Amaru
+
+mutable struct MeshCanvas<:ChartComponent
+    width::Float64
+    height::Float64
+    limits::Vector{Float64}
+    box::Vector{Float64}
+    function MeshCanvas()
+        return new(0.0, 0.0)
+    end
+end
+
+function configure!(chart::Chart, canvas::MeshCanvas)
+end
