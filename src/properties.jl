eaef096c09a222f4bb09f6024b0a0b7d5ed87553
Raul Durand <raul.durand@gmail.com>
2024-03-15 15:16:58 -0300

    comments removed

--- src/properties.jl
+++ src/properties.jl
@@ -1,8 +1,3 @@
 # This file is part of Amaru package. See copyright license in https://github.com/NumSoftware/Amaru
 
 abstract type ElemProperties end
-
-# @inline Base.:(<<)(a, b::ElemProperties) = return (a, b)
-# @inline Base.:(=>)(a, b::ElemProperties) = return (a, b)
-
-# @inline Base.:(<<)(a, b::Type{<:ElemProperties}) = return (a, b)
