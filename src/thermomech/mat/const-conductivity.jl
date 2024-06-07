2805c0ea9a4ea9198ae9f10f743647a12cfe2cbf
Raul Durand <raul.durand@gmail.com>
2024-06-07 18:51:27 -0300

    updated field Q

--- src/thermomech/mat/const-conductivity.jl
+++ src/thermomech/mat/const-conductivity.jl
@@ -6,13 +6,13 @@ export ConstConductivity
 mutable struct ConstConductivityState<:IpState
     env::ModelEnv
     ut::Float64
-    QQ::Array{Float64,1}
-    D::Array{Float64,1}
+    # q::Array{Float64,1}
+    Q::Array{Float64,1}
     function ConstConductivityState(env::ModelEnv)
         this = new(env)
         this.ut = 0.0
-        this.QQ  = zeros(env.ndim)
-        this.D  = zeros(env.ndim)
+        # this.q  = zeros(env.ndim)
+        this.Q  = zeros(env.ndim)
         return this
     end
 end
@@ -57,19 +57,19 @@ end
 
 function update_state!(mat::ConstConductivity, state::ConstConductivityState, Δut::Float64, G::Array{Float64,1}, Δt::Float64)
     K = calcK(mat, state)
-    state.QQ   = -K*G
-    state.D  += state.QQ*Δt
+    q = -K*G
+    state.Q  += q*Δt
     state.ut += Δut
-    return state.QQ
+    return q
 end
 
 
 function ip_state_vals(mat::ConstConductivity, state::ConstConductivityState)
     D = OrderedDict{Symbol, Float64}()
-    #D[:qx] = state.QQ[1]
-    #D[:qy] = state.QQ[2]
+    #D[:qx] = state.q[1]
+    #D[:qy] = state.q[2]
     #if state.env.ndim==3
-        #D[:qz] = state.QQ[3]
+        #D[:qz] = state.q[3]
     #end
     return D
 end
