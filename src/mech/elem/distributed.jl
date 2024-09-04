46ccfcfa60ed914eb64ce5c49ad107a029ea2299
Raul Durand <raul.durand@gmail.com>
2024-09-04 18:11:21 -0300

    fixed bu wx -> wz for 3d

--- src/mech/elem/distributed.jl
+++ src/mech/elem/distributed.jl
@@ -152,79 +152,6 @@ function mech_boundary_forces(elem::Element, facet::Cell, key::Symbol, val::Unio
 end
 
 
-# # Distributed natural boundary conditions for faces and edges of bulk elements
-# function mech_boundary_forces(elem::Element, facet::Cell, key::Symbol, val::Union{Real,Symbol,Expr})
-#     ndim  = elem.env.ndim
-#     suitable_keys = (:qx, :qy, :qz, :qn, :tx, :ty, :tz, :tn)
-#     isedgebc = key in (:qx, :qy, :qz, :qn) 
-    
-#     # Check keys
-#     key in suitable_keys || error("mech_boundary_forces: boundary condition $key is not applicable as distributed bc at element of type $(typeof(elem)). Suitable keys are $(string.(suitable_keys))")
-#     key in (:tz,:qz) && ndim==2 && error("mech_boundary_forces: boundary condition $key is not applicable in a 2D analysis")
-#     isedgebc && elem.env.ana.stressmodel==:axisymmetric && error("mech_boundary_forces: boundary condition $key is not applicable in a axisymmetric analysis")
-#     isedgebc && facet.shape.ndim==2 && error("mech_boundary_forces: boundary condition $key is not applicable on surfaces")
-#     !isedgebc && facet.shape.ndim==1 && ndim==3 && error("mech_boundary_forces: boundary condition $key is not applicable on 3D edges")
-
-#     th     = isedgebc ? 1.0 : elem.env.ana.thickness
-#     nodes  = facet.nodes
-#     nnodes = length(nodes)
-#     t      = elem.env.t
-
-#     # Calculate the facet coordinates 
-#     C = getcoords(nodes, ndim)
-
-#     # Vector with values to apply
-#     Q = zeros(ndim)
-
-#     # Calculate the nodal values
-#     F     = zeros(nnodes, ndim)
-#     shape = facet.shape
-#     ips   = get_ip_coords(shape)
-
-#     for i in 1:size(ips,1)
-#         R = ips[i].coord
-#         w = ips[i].w
-#         N = shape.func(R)
-#         D = shape.deriv(R)
-#         J = C'*D
-#         X = C'*N
-
-#         if ndim==2
-#             x, y = X
-#             vip = evaluate(val, t=t, x=x, y=y)
-#             Q = zeros(2)
-#             elem.env.ana.stressmodel==:axisymmetric && (th = 2*pi*X[1])
-#         else
-#             x, y, z = X
-#             vip = evaluate(val, t=t, x=x, y=y, z=z)
-#             Q = zeros(3)
-#         end
-
-#         if key in (:tx, :qx)
-#             Q[1] = vip
-#         elseif key in (:ty, :qy)
-#             Q[2] = vip
-#         elseif key in (:tz, :qz)
-#             Q[3] = vip
-#         elseif key in (:tn , :qn)
-#             if  ndim==2
-#                 n = [J[2], -J[1]]
-#             else
-#                 n = cross(J[:,1], J[:,2])
-#             end
-#             Q = vip*normalize(n)
-#         end
-
-#         coef = norm2(J)*w*th
-#         @mul F += coef*N*Q' # F is a matrix
-#     end
-
-#     # generate a map
-#     keys = (:ux, :uy, :uz)[1:ndim]
-#     map  = [ node.dofdict[key].eq_id for node in facet.nodes for key in keys ]
-
-#     return reshape(F', nnodes*ndim), map
-# end
 
 # Body forces for bulk elements
 function mech_solid_body_forces(elem::Element, key::Symbol, val::Union{Real,Symbol,Expr})
@@ -234,7 +161,7 @@ function mech_solid_body_forces(elem::Element, key::Symbol, val::Union{Real,Symb
 
     # Check keys
     key in suitable_keys || error("mech_solid_body_forces: condition $key is not applicable as distributed bc at element of type $(typeof(elem)). Suitable keys are $(string.(suitable_keys))")
-    (key == :wx && ndim==2) && error("mech_solid_body_forces: key $key is not applicable in a 2D analysis")
+    (key == :wz && ndim==2) && error("mech_solid_body_forces: key $key is not applicable in a 2D analysis")
 
     nodes  = elem.nodes
     nnodes = length(nodes)
@@ -289,60 +216,6 @@ function mech_solid_body_forces(elem::Element, key::Symbol, val::Union{Real,Symb
     return reshape(F', nnodes*ndim), map
 end
 
-
-# function mech_shell_boundary_forces(elem::Element, facet::Cell, key::Symbol, val::Union{Real,Symbol,Expr})
-#     ndim = 3
-#     suitable_keys = (:tx, :ty, :tz, :tn)
-
-#     # Check keys
-#     key in suitable_keys || error("mech_shell_boundary_forces: boundary condition $key is not applicable as distributed bc at element of type $(typeof(elem)). Suitable keys are $(string.(suitable_keys))")
-#     idx = key==:tx ? 1 : key==:ty ? 2 : key==:tz ? 3 : 0
-
-#     nodes  = facet.nodes
-#     nnodes = length(nodes)
-#     t      = elem.env.t
-
-#     # Force boundary condition
-#     nnodes = length(nodes)
-
-#     # Calculate the facet coordinates matrix
-#     C = getcoords(nodes, ndim)
-
-#     # Vector with values to apply
-#     Q = zeros(ndim)
-
-#     # Calculate the nodal values
-#     F     = zeros(nnodes, ndim)
-#     shape = facet.shape
-#     ips   = get_ip_coords(shape)
-
-#     for i in 1:size(ips,1)
-#         R = ips[i].coord
-#         w = ips[i].w
-#         N = shape.func(R)
-#         D = shape.deriv(R)
-#         J = C'*D
-#         X = C'*N
-
-#         x, y, z = X
-#         vip = evaluate(val, t=t, x=x, y=y, z=z)
-#         if key==:tn
-#             n = cross(J[:,1], J[:,2])
-#             Q = vip*normalize!(n)
-#         else
-#             Q[idx] = vip
-#         end
-
-#         coef = norm2(J)*w
-#         @mul F += coef*N*Q' # F is a matrix
-#     end
-
-#     # generate a map
-#     keys = (:ux, :uy, :uz)
-#     map  = [ node.dofdict[key].eq_id for node in facet.nodes for key in keys ]
-
-#     return reshape(F', nnodes*ndim), map
-# end
 
 
 function mech_shell_body_forces(elem::Element, key::Symbol, val::Union{Real,Symbol,Expr})
